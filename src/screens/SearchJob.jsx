af48ec9378db5a8e4d19828a423d95d36cd3c89f
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-04-07 17:21:16 +0500

    section

--- src/screens/SearchJob.jsx
+++ src/screens/SearchJob.jsx
@@ -255,6 +255,24 @@ export default function SearchJob() {
           </div>
         </div>
       </div>
+      <div className="search__job__section">
+        <div className="search__job__section__content">
+          <div className="search__job__section__content__heading">
+            Lorem Ipsum is simply dummy text
+          </div>
+          <div className="search__job__section__content__para">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially.
+          </div>
+          <button className="aigron__project__main__container__content__btn">
+            View More
+          </button>
+        </div>
+      </div>
     </>
   );
 }
